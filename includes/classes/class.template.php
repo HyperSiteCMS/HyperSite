84f25c226f60c522c66def4f2732ccdc181c72a6
Rayth <ramorrison1992@gmail.com>
2017-02-07 04:24:42 +0000

    Added variable assigning functions

--- includes/classes/class.template.php
+++ includes/classes/class.template.php
@@ -72,4 +72,77 @@ class template
         $this->_rootref = &$this->_data['.'][0];
         return true;
     }
+    //Assign a group of variables from an array
+    function assign_vars($array)
+    {
+        foreach ($array as $key => $value)
+        {
+            $this->_rootref[$key] = $value;
+        }
+        return true;
+    }
+    //Assign a single variable
+    function assign_var($name, $value)
+    {
+        $this->_rootref[$name] = $value;
+        return true;
+    }
+    //Assign an array as a block variable (array)
+    function assign_array($array_name, $array)
+    {
+        if (strpos($array_name, '.') !== false)
+        {
+            //Nested block
+            $blocks = explode('.', $array_name);
+            $block_count = sizeof($blocks) - 1;
+            
+            $str = &$this->_data;
+            for ($x = 0; $x < $block_count; $x++)
+            {
+                $str = &$str[$blocks[$x]];
+                $str = &$str[sizeof($str) - 1];
+            }
+            $row_count = isset($str[$blocks[$block_count]]) ? sizeof($str[$blocks[$block_count]]) : 0;
+            $array['ROW_COUNT'] = $row_count;
+            
+            //Assign first row
+            if (!$row_count)
+            {
+                $array['FIRST_ROW'] = true;
+            }
+            
+            //Always assign last row
+            $array['LAST_ROW'] = true;
+            if ($row_count > 0)
+            {
+                unset($str[$blocks[$block_count]][($row_count - 1)]['LAST_ROW']);
+            }
+            
+            //Now assign the actual array
+            $str[$blocks[$block_count]][] = $array;
+        }
+        else
+        {
+            //Top-level block
+            $row_count = (isset($this->_data[$array_name])) ? sizeof($this->_data[$array_name]) : 0;
+            $array['ROW_COUNT'] = $row_count;
+            
+            //Assign FIRST_ROW
+            if (!$row_count)
+            {
+                $array['FIRST_ROW'] = true;
+            }
+            
+            //Assign LAST_ROW and remove entry before
+            $array['LAST_ROW'] = true;
+            if ($row_count > 0)
+            {
+                unset($this->_data[$array_name][($row_count - 1)]['LAST_ROW']);
+            }
+            
+            //Add new iteration with variables given
+            $this->_data[$array_name][] = $array;
+        }
+        return true;
+    }
 }
